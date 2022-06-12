a6d640cddb95a694feacbb521e3a9c9ca38e4f09
Heather Shultz <silverlycold@gmail.com>
2022-06-12 17:23:25 -0500

    Created  welcome page for software.

---
+++ myConsoleProject/myConsoleProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we’re “accounting” on you!");
+        }
+    }
+}
