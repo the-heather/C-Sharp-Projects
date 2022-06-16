a6092de650476affc3bce9806ba6273a1cd3cbee
Heather Shultz <silverlycold@gmail.com>
2022-06-15 19:36:34 -0500

    created a while loop and do while loop

---
+++ ComparisonWhileStatements/ComparisonWhileStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ComparisonWhileStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int i = 0; // initialization
+
+            while (true) // while condition
+            {
+                Console.WriteLine("i = {0}", i);
+
+                i++; // increment
+
+                if (i > 5)
+                    break; //exit loop
+            }
+
+            int n = 0; // initialization
+
+            do // do while loop executes the code block at least once
+            {
+                Console.WriteLine("n = {0}", n);
+                n++; // increment
+
+            } while (n < 5); //condition
+        }
+    }
+}
