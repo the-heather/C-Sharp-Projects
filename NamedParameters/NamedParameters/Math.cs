f660956f5afa8939ab482790e3515e15a8363d73
Heather Shultz <silverlycold@gmail.com>
2022-06-22 20:36:35 -0500

    created named parameters

---
+++ NamedParameters/NamedParameters/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameters
+{
+    class Math
+    {
+        public static void Multi(int firstNum, int secondNum)//multiply method 
+        {
+            int mAnswer = firstNum * 13; //multiply 1st # by 13
+            Console.WriteLine(secondNum); //display 2nd # to console
+        }
+    }
+}
---
+++ NamedParameters/NamedParameters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NamedParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math.Multi(34, 25); //calling method
+            Math.Multi(firstNum: 47, secondNum: 25); //calling method with named parameters.
+
+        }
+        }
+}
