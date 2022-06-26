8521a4bfa618083e145f2e463373e0eafa9c1161
Heather Shultz <silverlycold@gmail.com>
2022-06-26 15:53:20 -0500

    need help

---
+++ UsingGenerics/UsingGenerics/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingGenerics
+{
+    class Employee<T>
+    {
+      public List<T> Things { get; set; }
+    }
+}
---
+++ UsingGenerics/UsingGenerics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> first = new Employee<string>();
+            //no idea what to do next (^_^ ;)
+
+
+            Employee<int> second = new Employee<int>();
+
+        }
+    }
+}
