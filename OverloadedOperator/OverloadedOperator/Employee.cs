bf4e4ebbc0f29e5c4e5a5bcdfe952e4c2b584e3c
Heather Shultz <silverlycold@gmail.com>
2022-06-27 20:56:05 -0500

    Overloaded == operator

---
+++ OverloadedOperator/OverloadedOperator/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OverloadedOperator
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Id { get; set; }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+        }
+    }
+
+}
---
+++ OverloadedOperator/OverloadedOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OverloadedOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Sample";
+            employee1.LastName = "Student";
+            employee1.Id = 1342;
+
+
+            Employee employee2 = new Employee();
+            employee2.FirstName = "Sample";
+            employee2.LastName = "Student";
+            employee2.Id = 1342;
+
+            Console.WriteLine(employee1 == employee2);
+        }
+    }
+}
