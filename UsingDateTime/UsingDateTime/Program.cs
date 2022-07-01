9b6e5e4b83bbb2ed3d26378220a2ae189065c6e2
Heather Shultz <silverlycold@gmail.com>
2022-07-01 18:57:13 -0500

    used DateTime when logging a value to a text file and displayed it to the user.

---
+++ UsingDateTime/UsingDateTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace UsingDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine(DateTime.Now);
+                Console.WriteLine("Please enter a number: ");
+                string value = Convert.ToString(Console.ReadLine());
+                using (StreamWriter file = new StreamWriter(@"C:\Users\Admin\Documents\log.txt", true))
+                {
+                    file.WriteLine(DateTime.Now);
+                    file.WriteLine(value);
+                }
+                string txtFile = File.ReadAllText(@"C:\Users\Admin\Documents\log.txt");
+                Console.WriteLine(txtFile);
+            }
+        }
+    }
+}
