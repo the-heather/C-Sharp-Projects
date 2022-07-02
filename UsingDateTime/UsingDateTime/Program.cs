12eca3325c8f650c216e5c9774521d6e7b1c9a2a
Heather Shultz <silverlycold@gmail.com>
2022-07-01 19:50:53 -0500

    take user input and add it to the current time, then display the result.

--- UsingDateTime/UsingDateTime/Program.cs
+++ UsingDateTime/UsingDateTime/Program.cs
@@ -7,19 +7,17 @@ namespace UsingDateTime
     {
         static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                Console.WriteLine(DateTime.Now);
-                Console.WriteLine("Please enter a number: ");
-                string value = Convert.ToString(Console.ReadLine());
-                using (StreamWriter file = new StreamWriter(@"C:\Users\Admin\Documents\log.txt", true))
-                {
-                    file.WriteLine(DateTime.Now);
-                    file.WriteLine(value);
-                }
-                string txtFile = File.ReadAllText(@"C:\Users\Admin\Documents\log.txt");
-                Console.WriteLine(txtFile);
-            }
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number: ");
+            // store input in value varable
+            int value = Convert.ToInt32(Console.ReadLine());
+            //store current datetime in varable
+            DateTime dt = DateTime.Now;
+            // store a new time span equal to user input (hrs, mins, secs)
+            TimeSpan ts = new TimeSpan(value, 0, 0);
+            // add timespan to current time
+            DateTime value2 = dt.Add(ts);
+            Console.WriteLine(value2);
         }
     }
 }
