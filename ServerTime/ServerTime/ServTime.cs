5239bb40d28bb753c748bed82af384d818c17d65
Heather Shultz <silverlycold@gmail.com>
2022-07-03 22:52:51 -0500

    Need Help

---
+++ ServerTime/ServerTime/ServTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace ServerTime
+{
+    public class ServTime
+    {
+        private readonly string connectionString = @"Data Source = DESKTOP - HN3S8JL; Initial Catalog = ServTime; Integrated Security = True";
+         using (SqlConnection connection = new SqlConnection(connectionString))
+        SqlServer.GETDATE();
+
+
+
+
+    }
+}
